05f4a9cf38e18cb4af2fc39534a92beb351cef87
SevendaysZzzz <90546606+SevendaysZzzz@users.noreply.github.com>
2022-07-12 11:46:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,8 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.122.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.122.1/g' package/base-files/files/bin/config_generate
 
 # Modify default 8M flash to 32M
-sed -i '831s/7872k/32768k/' target/linux/ramips/image/mt7620.mk
+sed -i '/define\ Device\/phicomm_psg1218a/,/TARGET_DEVICES\ \+=\ phicomm_psg1218a/s/7872k/32768k/g' target/linux/ramips/image/mt7620.mk
 sed -i 's/0x7b0000/0x1fb0000/' target/linux/ramips/dts/mt7620a_phicomm_psg1218a.dts
